ea7ede8306bb8953c72cc7815c4fc5f7d1a490e6
vmastr <143725645+vmastr@users.noreply.github.com>
2024-02-03 13:52:19 -0500

    Update generate_orderbook.py

--- generate_orderbook.py
+++ generate_orderbook.py
@@ -11,37 +11,34 @@ import random
 def generate_orderbook(N, M):
     omega = np.random.normal(0, 1, N)
     omega = abs(omega)
-    nij   = np.random.geometric(p=1/(1+omega) ,size=(M,N)) -1
+    nij = np.random.geometric(p=1 / (1 + omega), size=(M, N)) - 1
 
     orderbook = []
-    for i in range(M): 
-        orderbook.append(np.concatenate([[j]*nij[i,j] for j in range(N)]))
-    
+    for i in range(M):
+        orderbook.append(np.concatenate([[j] * nij[i, j] for j in range(N)]))
+
     return orderbook
 
 # given N number of items, which is assumed to be divisible by 4, output a list of coordinates on the square
 # this functions returns a list
 def position_function(N):
     intN = int(N/4)
-    a1 =  [ (i+1 , 0) for i in range(intN)]    # bottom side
-    a2 = [ (intN, i+1) for i in range(intN)]   # right side
-    a3 = [ (0, i+1) for i in range(intN)]      # left side
-    a4 = [ (i+1, intN) for i in range(intN-1)] # top side
+    a1 =  [(i+1 , 0) for i in range(intN)]    # bottom side
+    a2 = [(intN+1, i+1) for i in range(intN)]   # right side
+    a3 = [(0, i+1) for i in range(intN)]      # left side
+    a4 = [(i+1, intN+1) for i in range(intN)] # top side
     a = [*a1, *a2, *a3, *a4]                   # concat the sides together
     return a
 
-# given N number of items, return  which is assumed to be divisible by 4, return a list of randomised coordinates
-def position_random_generator(N):
-    positions = position_function(N)         # generate positions ordered in a certain manner
-    random.shuffle(positions)                # shuffle the items in the list
-    return positions
 
-def dictionary_func(N): # come up with a dictionary given N number of items. It assigns the item to a position in a naive way.
+def dictionary_func(N):
+# come up with a dictionary given N number of items. It assigns the item to a position in a naive way.
     a = position_function(N)
-    diction = {i: a[i] for i in range(N-1)}
+    diction = {i: a[i] for i in range(N)}
     return diction
 
-def OrderToCoord(order, N): # given order and number of items, return the coordinates of these items in a list
+def OrderToCoord(order, N):
+# given order and number of items, return the coordinates of these items in a list
     diction_tr = dictionary_func(N)
     coord = [diction_tr[item] for item in order]
     return coord
